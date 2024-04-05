ec0f1f05655dc9f19bdde49757692443d7ebefce
Dan <dp4006@ship.edu>
2024-04-05 01:53:55 -0400

    fixed bug where you can open up the pause menu in the options menu twice, added escape key to go back in options menu

--- Scripts/GameController.gd
+++ Scripts/GameController.gd
@@ -4,6 +4,8 @@ extends Node
 var is_game_paused = false
 var is_main_menu_visible = false
 var is_pause_menu_visible = false
+var is_death_menu_visible = false
+var is_options_menu_visible = false
 
 # References to scenes (preloaded)
 var main_menu_scene = preload("res://Scenes/main_menu.tscn")
@@ -24,6 +26,7 @@ var pause_menu
 var options_menu
 var death_menu
 
+# Other instances
 var player_instance
 
 var back_destination
@@ -33,13 +36,15 @@ func _ready():
 	show_main_menu()
 	
 func _process(delta):
-	if is_main_menu_visible == false:
+	if not is_main_menu_visible and not is_death_menu_visible and not is_options_menu_visible:
 		if Input.is_action_just_pressed("ui_escape"):
-			print("pressed!")
 			if is_pause_menu_visible == true:
 				hide_pause_menu()
 			else:
 				show_pause_menu()
+	if is_options_menu_visible:
+		if Input.is_action_just_pressed("ui_escape"):
+			_go_back()
 
 func init_menus():
 	main_menu = main_menu_scene.instantiate()
@@ -55,6 +60,7 @@ func init_menus():
 	ui_layer_pause_menu = get_node("/root/main/CanvasLayerPauseMenu")
 	ui_layer_pause_menu.add_child(pause_menu)
 	pause_menu.hide()
+	# Connections for pause menu signals
 	pause_menu.connect("resume_game", _on_resume_game)
 	pause_menu.connect("show_options", _on_show_options_from_pause)
 	pause_menu.connect("go_main", _recreate_main_scene)
@@ -64,6 +70,7 @@ func init_menus():
 	ui_layer_options_menu = get_node("/root/main/CanvasLayerOptionsMenu")
 	ui_layer_options_menu.add_child(options_menu)
 	options_menu.hide()
+	# Connections for options menu signals
 	options_menu.connect("go_back", _go_back)
 	options_menu.connect("apply_changes", _apply_changes)
 	
@@ -71,6 +78,7 @@ func init_menus():
 	ui_layer_death_menu = get_node("/root/main/CanvasLayerDeathMenu")
 	ui_layer_death_menu.add_child(death_menu)
 	death_menu.hide()
+	# Connections for death menu signals
 	death_menu.connect("respawn_player", _respawn_player)
 	death_menu.connect("go_main", _recreate_main_scene)
 	death_menu.connect("quit_game", _on_quit_game)
@@ -79,6 +87,7 @@ func _respawn_player():
 	hide_death_menu()
 	remove_player()
 	spawn_player()
+	is_death_menu_visible = false
 
 func _go_back():
 	hide_options()
@@ -91,6 +100,14 @@ func _go_back():
 	
 func _apply_changes():
 	pass
+	
+func _on_quit_game():
+	get_tree().quit()
+
+func _open_death_menu():
+	show_death_menu()
+	death_menu.focus()
+	is_death_menu_visible = true
 
 func _recreate_main_scene():
 	hide_death_menu() # if its shown
@@ -100,13 +117,30 @@ func _recreate_main_scene():
 
 func _on_resume_game():
 	hide_pause_menu()
-	
-func _on_show_main():
-	hide_pause_menu()
 
+func _on_start_game():
+	hide_main_menu()
+	spawn_player()
+
+func _on_show_options_from_main():
+	back_destination = "main_menu"
+	hide_main_menu()
+	options_menu.show()
+	options_menu.focus()
+	is_options_menu_visible = true
+	
+func _on_show_options_from_pause():
+	back_destination = "pause_menu"
+	hide_pause_menu()
+	set_pause_state(true)
+	options_menu.show()
+	options_menu.focus()
+	is_options_menu_visible = true
+	
 func show_main_menu():
 	main_menu.show()
 	is_main_menu_visible = true
+	main_menu.focus()
 
 func hide_main_menu():
 	main_menu.hide()
@@ -117,39 +151,21 @@ func show_pause_menu():
 	is_game_paused = true
 	is_pause_menu_visible = true
 	set_pause_state(true)
+	pause_menu.focus()
 	
 func hide_pause_menu():
 	pause_menu.hide()
 	is_game_paused = false
 	is_pause_menu_visible = false
 	set_pause_state(false)
-
-func _on_start_game():
-	hide_main_menu()
-	spawn_player()
-
-func _on_show_options_from_main():
-	back_destination = "main_menu"
-	hide_main_menu()
-	options_menu.show()
-	
-func _on_show_options_from_pause():
-	back_destination = "pause_menu"
-	hide_pause_menu()
-	set_pause_state(true)
-	options_menu.show()
 	
 func hide_options():
 	options_menu.hide()
-
-func _on_quit_game():
-	get_tree().quit()
-
-func _open_death_menu():
-	show_death_menu()
+	is_options_menu_visible = false
 	
 func show_death_menu():
 	death_menu.show()
+	death_menu.focus()
 	
 func hide_death_menu():
 	death_menu.hide()
